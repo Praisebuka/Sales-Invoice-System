928adfd915c4e4fa78f8399ce44dc0bfc93d711e
Praise Ebuka <praise.njoga@gmail.com>
2024-06-15 21:02:12 +0100

    components

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,6 +26,11 @@
     <link rel="stylesheet" type="text/css" href="{{asset('/')}}css/main.css">
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        .required-icon {
+            color: red;
+        }
+    </style>
 </head>
 <body class="app sidebar-mini rtl">
 <!-- Navbar-->
