86d6c75baa6b548bbe04be702058a570e444e96c
Praise Ebuka <praise.njoga@gmail.com>
2024-06-16 19:06:25 +0100

    smooth

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,11 +10,12 @@
                 <img width="30px" class="app-sidebar__user-avatar" src="{{ asset('images/user/'.Auth::user()->image) }}" alt="User Image"> 
             </a>
             <ul class="dropdown-menu settings-menu dropdown-menu-right">
-                <!-- <li><a class="dropdown-item" href="{{route('update_password')}}"><i class="fa fa-cog fa-lg"></i>Password</a></li> -->
                 <li><a class="dropdown-item" href="{{route('edit_profile')}}"><i class="fa fa-user fa-lg"></i> Profile </a></li>
-                <li><a class="dropdown-item" href="{{ route('logout') }}"
-                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();"><i class="fa fa-sign-out fa-lg"></i> Logout</a>
+                <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fa fa-sign-out fa-lg"></i> 
+                        Logout
+                    </a>
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
