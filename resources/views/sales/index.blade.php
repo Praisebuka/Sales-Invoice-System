e38e767a1ae66c0c8c5135ddd08dab52436903e7
Praise Ebuka <praise.njoga@gmail.com>
2024-11-14 15:13:28 +0100

    sales

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -10,16 +10,16 @@
     <main class="app-content">
         <div class="app-title">
             <div>
-                <h1><i class="fa fa-th-list"></i> Sales Table</h1>
+                <h1><i class="fa fa-th-list"></i> Sales Table </h1>
             </div>
             <ul class="app-breadcrumb breadcrumb side">
                 <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
                 <li class="breadcrumb-item">Sales</li>
-                <li class="breadcrumb-item active"><a href="#">Sales Table</a></li>
+                <li class="breadcrumb-item active"><a href="#"> Sales Table </a></li>
             </ul>
         </div>
         <div class="">
-            <a class="btn btn-primary" href="{{route('invoice.create')}}"><i class="fa fa-plus"></i> Create New Invoice</a>
+            <a class="btn btn-primary" href="{{route('invoice.create')}}"><i class="fa fa-plus"></i> Create New Invoice </a>
         </div>
 
         <div class="row mt-2">
@@ -29,25 +29,25 @@
                         <table class="table table-hover table-bordered" id="sampleTable">
                             <thead>
                             <tr>
-                                <th>Product </th>
-                                <th>Qty </th>
-                                <th>Price</th>
-                                <th>Total</th>
-                                <th>Date </th>
+                                <th> Product </th>
+                                <th> Qty </th>
+                                <th> Price </th>
+                                <th> Total </th>
+                                <th> Date </th>
                             </tr>
                             </thead>
                             <tbody>
-            @foreach($sales as $sale)
-                <tr>
-                    <td>{{ $sale->product->name }}</td>
-                    <td>{{ $sale->qty }}</td>
-                    <td>{{ $sale->price }}</td>
-                    <td>{{ $sale->amount }}</td>
-                    <td>{{ $sale->created_at }}</td>
-                    
-                </tr>
-            @endforeach
-        </tbody>
+                                @foreach($sales as $sale)
+                                    <tr>
+                                        <td>{{ $sale->product->name }}</td>
+                                        <td>{{ $sale->qty }}</td>
+                                        <td>{{ $sale->price }}</td>
+                                        <td>{{ $sale->amount }}</td>
+                                        <td>{{ $sale->created_at }}</td>
+                                        
+                                    </tr>
+                                @endforeach
+                            </tbody>
                         </table>
                     </div>
                 </div>
