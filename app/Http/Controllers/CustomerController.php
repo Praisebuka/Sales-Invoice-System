87b3fcf6376891bc35a659719e91726f7475b0cb
Praisebuka <praise.njoga@gmail.com>
2024-12-10 16:38:15 +0100

    k

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -16,7 +16,7 @@ class CustomerController extends Controller
 
     public function index()
     {
-        $customers = Customer::all();
+        $customers = Customer::all();  
         return view('customer.index', compact('customers'));
     }
 
